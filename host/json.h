923657e830ae55c3ea89ce6a8895c0d359d9ee52
jacob lindborg <65565795+s5msepi0l@users.noreply.github.com>
2023-09-18 23:08:49 +0300

    Update json.h
    implemented a somewhat primitive json parser needs considerable work for full functionality

--- host/json.h
+++ host/json.h
@@ -2,18 +2,133 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
-#include <list>
-#include <map>
-#include <variant>
+#include <utility>
+#include <stack>
+#include <unordered_map>
 
-using JSONValue = std::variant<std::string, double, bool, std::nullptr_t, std::vector<JSONValue>, std::map<std::string, JSONValue>>;
+inline bool is_digit(char src) {
+	return (src >= '0' && src <= '9') ? true : false;
+}
 
-class JSON {
-private:
+inline bool m_is_digit(const char *src) {
+	return ((*src >= '0' && *src <= '9') || (*(src+1) >= '0' && *(src+1) <= '9')) ? true : false;
+}
 
-public:
-	JSONValue parse
 
-private:
+namespace JSON {
+	union JSON_VAL;
+
+	typedef union JSON_VAL{
+		std::string str;
+		long u64;
+		int u32;
+		char u8;
+		//std::pair<JSON_VAL, JSON_VAL> obj;
+		JSON_VAL() : str() { }
+		~JSON_VAL() {} // OOP shit like this makes me want to live in a cabin in the woods and become amish
+	}JSON_VAL;
+
+	typedef struct JSON_OBJ{
+		JSON_VAL val;
+		JSON_OBJ* next;
+
+		JSON_OBJ(): val(), next(nullptr) {}
+		~JSON_OBJ() {
+			JSON_OBJ* tmp;
+			while (next->next != nullptr) {
+				tmp = next;
+				next = next->next;
+				delete tmp;
+			}
+
+			delete next;
+		}
+	}JSON_OBJ;
+
+	class JSON_Codec {
+	private:
+		std::unordered_map<char, char> order;
+	public:
+		JSON_Codec() {
+			order[']'] = '[';
+			order['}'] = '{';
+		}
+		
+		JSON_OBJ *parse(std::string input) {
+			std::stack<char> brackets;
+			JSON_OBJ *buffer = new JSON_OBJ();
+			JSON_OBJ *front = buffer;
+	
+			for (int i = 0; i < input.size(); i++) {
+				switch (input[i]) {
+					case ' ':
+						continue;
+
+					case '"':
+						while (input[++i] != '"') {
+							buffer->val.str.push_back(input[i]);
+						}
+						break;
+
+					case ',':
+						buffer->next = new JSON_OBJ();
+						buffer = buffer->next;
+						break;
+
+					case '[':
+						brackets.push('[');
+						break;
+
+					case ']':
+						std::cout << "check" << std::endl;
+						if (brackets.top() != '[')
+							goto error;
+						else
+							brackets.pop();
+						break;
+					
+					case '{':
+						brackets.push('{');
+						break;
+
+					case '}':
+						std::cout << "check" << std::endl;
+						if (brackets.top() != '{')
+							goto error;
+						else
+							brackets.pop();
+						break;
+
+					case ':':
+						break; 
+
+					default: //current value is either boolean or integer
+						if (m_is_digit(input.c_str())) { // is integer 
+							long number = 0;
+							for (int j = i; j < input.size(); j++) {
+
+							}
+						}
+
+						break;
+				}
+			}
+			if (brackets.size() != 0)
+				goto error;
+
+			return front;
+			error:
+				std::cout << "ERROR PARSING JSON FILE\n";
+				// parsing error
+				return front;
+		}
+
+		void display(JSON_OBJ* head) {
+			JSON_OBJ* next = head;
+			while (next != nullptr) {
+				std::cout << next->val.str << std::endl;
+				next = next->next;
+			}
+		}
+	};
 };
