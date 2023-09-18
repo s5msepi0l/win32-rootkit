1c5a4bd5566cff8f3ee2dcc2547e495a8906c5d9
jacob lindborg <65565795+s5msepi0l@users.noreply.github.com>
2023-09-18 23:09:08 +0300

    Update common.h

--- host/common.h
+++ host/common.h
@@ -1,5 +1,7 @@
 #pragma once
+
 #define WIN32_LEAN_AND_MEAN
+#define uint8 unsigned char
 #define uint16 unsigned short
 #define uint64 unsigned long
 
@@ -7,6 +9,7 @@
 #include <vector>
 #include <fstream>
 #include <tchar.h>
+#include <sstream>
 #include <string>
 
 #include <Windows.h>
@@ -17,17 +20,34 @@
 #include <WS2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
-std::string read_f(const char *path)
-{
-	std::ifstream fd(path);
-	std::string res;
+// should only be called once
+extern WSADATA _wsa;
+inline void init_wsa() {
+	if (WSAStartup(MAKEWORD(2, 2), &_wsa) != 0)
+		throw std::runtime_error("Unable to initialize WSA\n");
+}
 
-	if (fd.is_open())
-	{
-		fd >> res;
-		fd.close();
+inline void exit_wsa() {
+	WSACleanup();
+}
+
+// For the love of god never change this or binary file reading will be fucked
+std::string read_f(const char *path) {
+	std::ifstream file(path, std::ios::binary);
+
+	if (!file.is_open()) 
+		throw std::runtime_error("Unable to open file:");
+
+	std::string buffer;
+	unsigned char byte;
+
+	while (file.read(reinterpret_cast<char*>(&byte), sizeof(byte))) {
+		buffer.push_back(byte);
 	}
-	return res;
+
+	file.close();
+
+	return buffer;
 }
 
 bool write_f(std::string content, const char* path) 
@@ -42,4 +62,11 @@ bool write_f(std::string content, const char* path)
 	}
 
 	return false;
+}
+
+// mainly used as utility for packet chunking
+inline std::string hexify(long val) {
+	std::stringstream stream;
+	stream << std::hex << val;
+	return stream.str();
 }
